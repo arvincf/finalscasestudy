e44ce2b28ac4d2bffc69e5d963a755c97bebbc5e
arvincf <faustinoarvinclyde18@gmail.com>
2024-06-12 17:07:00 +0800

    databaseseeder update

--- finals-app/database/seeders/DatabaseSeeder.php
+++ finals-app/database/seeders/DatabaseSeeder.php
@@ -18,5 +18,21 @@ class DatabaseSeeder extends Seeder
             "password" => Hash::make("Abcd12345"),
             "userType" => "doctor"
         ]);
+
+        User::create([
+            "name" => "Patient",
+            "email" => "b@gmail.com",
+            "email_verified_at" => now(),
+            "password" => Hash::make("Bibi1234"),
+            "userType" => "patient"
+        ]);
+
+        User::create([
+            "name" => "Admin",
+            "email" => "c@gmail.com",
+            "email_verified_at" => now(),
+            "password" => Hash::make("Admin1234"),
+            "userType" => "admin"
+        ]);
     }
 }
